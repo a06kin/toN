f0c337534a83ead16cd89ba5f80caaaff4f3eecf
Aleksandr Anohin <a06kin@gmail.com>
2015-01-22 17:24:44 +0200

    height

--- src/lv/aaa/Main.java
+++ src/lv/aaa/Main.java
@@ -8,17 +8,24 @@ import java.util.ArrayList;
 public class Main {
 
     static ArrayList<Node> marked = new ArrayList<Node>();
+    static int maxLimbSize = 0;
+    static int limbSize = 0;
 
     public static void main(String[] args) {
     }
-    
+
     public static void dfs(Tree tree, Node now){
+        ++limbSize;
+        if (limbSize > maxLimbSize){
+            maxLimbSize = limbSize;
+        }
         marked.add(now);
 
-        for(Node tmp : now.getChildren()) {
-            if (!marked.contains(tmp)) {
+        for(Node tmp : now.getChildren()){
+            if (!marked.contains(tmp)){
                 dfs(tree, tmp);
             }
         }
+        --limbSize;
     }
 }
