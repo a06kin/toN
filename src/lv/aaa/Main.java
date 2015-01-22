a130fe06a3ebaeca1059e0737504d337275a3b2f
Aleksandr Anohin <a06kin@gmail.com>
2015-01-22 17:26:27 +0200

    example for Tree height

--- src/lv/aaa/Main.java
+++ src/lv/aaa/Main.java
@@ -12,6 +12,22 @@ public class Main {
     static int limbSize = 0;
 
     public static void main(String[] args) {
+        treeHight();
+    }
+
+    public static void treeHight(){
+	    Tree testTree = new Tree();
+        testTree.addNode("top", null);
+        testTree.addNode("second1", "top");
+        testTree.addNode("th1", "second1");
+        testTree.addNode("fo1", "th1");
+        testTree.addNode("fi1", "fo1");
+        testTree.addNode("second2", "top");
+        testTree.addNode("th2", "second2");
+        testTree.addNode("fo2", "th2");
+
+        dfs(testTree, testTree.getNodes().get("top"));
+        System.out.println("size = " + maxLimbSize);
     }
 
     public static void dfs(Tree tree, Node now){
